d699a7c8fe9958a84f308109597788fb8d66e5b3
MIN0225 <cro77@naver.com>
2025-04-30 23:25:37 +0900

    first commit

---
+++ my-react-app/README.md
@@ -0,0 +1,2 @@
+JavaScript Mastery 리액트 강의 따라하기
+유튜브 링크: [https://www.youtube.com/watch?v=dCLhUialKPQ&t=308s]
---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,74 @@
+import {useEffect, useState} from 'react';
+import Search from "./components/Search.jsx";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState('I AM BATMAN');
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMovies = async() => {
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if(!response.ok) {
+                throw new Error('Failed to fetch movie');
+            }
+
+            const data = await response.json();
+
+            if(data.Response === 'False') {
+                setErrorMessage(data.Error || 'Failed to fetch movie');
+                setMovieList([]);
+                return;
+            }
+
+            setMovieList(data.results || []);
+        } catch(error) {
+            console.log(`Error fetching movies: ${error}`);
+            setErrorMessage(error.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+  return (
+      <main>
+
+        <div className="pattern" />
+
+        <div className="wrapper">
+          <header>
+              <img src="./hero.png" alt="Hero Banner"/>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            <section className="all-movies">
+                <h2>All Movies</h2>
+
+                {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            </section>
+        </div>
+      </main>
+  );
+};
+
+export default App;
